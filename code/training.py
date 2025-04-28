e65c9c2595ce3a09484671471f7b1127e4394809
Aarfdark <aadarsh.natarajan@gmail.com>
2025-04-28 10:58:48 -0400

    whatever

--- code/training.py
+++ code/training.py
@@ -1,7 +1,11 @@
 import tensorflow as tf
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    print()
+    df = pd.read_csv("FLIC.csv")
+    print(df)
 
 if __name__ == "__main__":
     main()
